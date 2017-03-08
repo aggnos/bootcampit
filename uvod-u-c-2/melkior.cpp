cdaf666b56bbac3e841b7fb4e89cae7136a11ad1
aggnos <dotonkovi@gmail.com>
2017-03-08 14:32:24 +0100

    melkior+

--- uvod-u-c-2/melkior.cpp
+++ uvod-u-c-2/melkior.cpp
@@ -8,7 +8,14 @@ int main() {
   float a, s, d, f, g;
   cin >> a >> s >> d >> f >> g;
   float ukupno = (a+ 2*s + 3*d + 4*f + 5*g)/(a+s+d+f+g);
-  printf
+  printf("%.2f\n", ukupno);
+
+  if(ukupno < 2.00 || a > 0) cout << "nedovoljan" << endl;
+  else if (ukupno >= 2.00 && ukupno < 2.50) cout << "dovoljan" << endl;
+  else if (ukupno >= 2.50 && ukupno < 3.50) cout << "dobar" << endl;
+  else if (ukupno >= 3.50 && ukupno < 4.50) cout << "vrlo dobar" << endl;
+  else cout << "odlican" << endl;
+  
   return 0;
   
 }
