cb1b1b05b00b07fa9b471205e33e7217246d0b7e
aggnos <dotonkovi@gmail.com>
2017-03-13 12:09:19 +0100

    commit

--- uvod-u-c-4/prosjek_u_nizu.cpp
+++ uvod-u-c-4/prosjek_u_nizu.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int zbrajanje(int broj) {
-  int b;
-  cin >> b;
+int zbrajanje(int broj, int b) {
   return broj+b;
 }
 
 int main(int argc, char *argv[])
 {
-  int a, zbroj = 0;
+  int a, b, zbroj = 0;
   cin >> a;
   for (int i = 0;i < a; i++) {
-    zbroj = zbrajanje(zbroj);
+    cin >> b;
+    zbroj = zbrajanje(zbroj, b);
   }
   cout << zbroj/a << endl;
   return 0;
