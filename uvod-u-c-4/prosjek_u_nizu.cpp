e7677a3a8ca797baff04c60770aa90d5065cbbd0
aggnos <dotonkovi@gmail.com>
2017-03-13 12:11:34 +0100

    commit final

--- uvod-u-c-4/prosjek_u_nizu.cpp
+++ uvod-u-c-4/prosjek_u_nizu.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int zbrajanje(int broj, int b) {
+float zbrajanje(int broj, int b) {
   return broj+b;
 }
 
 int main(int argc, char *argv[])
 {
-  int a, b, zbroj = 0;
+  float a, b, zbroj = 0;
   cin >> a;
   for (int i = 0;i < a; i++) {
     cin >> b;
