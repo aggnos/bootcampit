e507030a4698310d7f44108aed3f48d7920eb790
aggnos <dotonkovi@gmail.com>
2017-03-13 10:47:41 +0100

    commit test

---
+++ uvod-u-c-4/krizic-kruzic.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool check (string polje1, string polje2, string polje3){
+  bool point = false;
+  return point;
+}
+
+int main(){
+  int counter = 1;
+  string kk;
+  string polje1, polje2, polje3;
+
+  while(check(polje1,polje2,polje3) == false) {
+    cin >> kk;
+    
+    if (counter%2 == 0)
+      
+    else {}
+    
+    counter++;
+  }
+
+  return 0;
+}
