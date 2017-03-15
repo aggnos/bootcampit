d3a93cccb9143eae9c3bb918e75d4a85829fbb5a
aggnos <dotonkovi@gmail.com>
2017-03-15 11:18:10 +0100

    quicksort

---
+++ sort/dinamite.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void quickSort(int arr[], int left, int right)
+{
+  int i = left, j = right;
+  int tmp;
+  int pivot = arr[(left + right) / 2];
+
+  /* partition */
+  while (i <= j) {
+    while (arr[i] < pivot)
+      i++;
+    while (arr[j] > pivot)
+      j--;
+    if (i <= j) {
+      tmp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tmp;
+      i++;
+      j--;
+    }
+  }
+  /* recursion */
+  if (left < j)
+    quickSort(arr, left, j);
+  if (i < right)
+    quickSort(arr, i, right);
+}
+
+int main(int argc, char *argv[])
+{
+
+  int n;
+  cin >> n;
+  int polje[n];
+  
+  for(int i = 0; i < n; i++) {
+    cin >> polje[i];
+  }
+  for(int i = 0; i < n; i++) {
+    cout << polje[i] << endl;
+  }
+
+  quickSort(polje, 0, n-1);
+  cout << "AFTER SORT: \n";
+  for(int i = 0; i < n; i++) {
+    cout << polje[i] << endl;
+  }
+  
+  
+  return 0;
+}
